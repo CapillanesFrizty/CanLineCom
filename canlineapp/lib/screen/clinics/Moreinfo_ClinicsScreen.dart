b0a613ce04ea206608a3adf5d8f6efcc86d0d26e
Kevin Kurt Intong <kintong_210000001981@uic.edu.ph>
2024-09-24 12:46:09 +0800

    Clinics Update

---
+++ canlineapp/lib/screen/clinics/Moreinfo_ClinicsScreen.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class MoreinfoClinicsscreen extends StatelessWidget {
+  const MoreinfoClinicsscreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
